430366cc998d168e83a2d046b11cbbb3f7d63eb2
Rakesh Nagarkoti <rakesh.wNv@gmail.com>
2017-10-04 23:33:11 +0530

    -

---
+++ Beauty of Generics/readme.md
@@ -0,0 +1,5 @@
+# GENERICS #
+
+Generics makes a developer's life easy* if used properly. Using generics forces the compiler to do type checks so that an Integer instance/object is NEVER passed where a String should be passed.. & vice versa.. and so on...   
+
+ 
---
+++ PlayWithStrings/Filepath.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String str = "/home/kenny/desktop/example.java";
+
+        int dot = str.lastIndexOf(".");
+        String extension = str.substring(str.lastIndexOf("."));
+        String fileName = str.substring(str.lastIndexOf("/") + 1, dot);
+        String fullPath = str.substring(0, str.lastIndexOf("/"));
+
+        System.out.println("File extension is: " + extension);
+        System.out.println("Filename is: " + fileName);
+        System.out.println("File path is: " + fullPath);
+    }
+}
+
